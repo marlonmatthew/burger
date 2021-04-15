f7d8ad9accba09bd906f09b84179728dbddf2268
Marlon Matthew <mbm.drums@gmail.com>
2021-04-15 15:58:22 -0700

    setup the db. created schema.sql. created seeds.sql.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cantina Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', FALSE);
