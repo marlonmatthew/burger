a0a2bef6aaea03f83623e1552be4aeba35c263aa
Marlon Matthew <mbm.drums@gmail.com>
2021-04-15 18:07:13 -0700

    added code for burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,37 @@ const express = require("express");
 const burger = require("../models/burger.js");
 const router = express.Router();
 
-//setup routes
+router.get("/", (req, res) => {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", (req, res) => {
+  burger.all((burgerData) => {
+    const burgerObject = {
+      burger_data: burgerData,
+    };
+    console.log(burgerObject);
+    res.render("index", burgerObject);
+  });
+});
+
+router.post("/burgers/create", (req, res) => {
+  burger.create(req.body.burger_name, (result) => {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+router.put("/burgers/:id", (req, res) => {
+  const burgerCondition = `id = ${req.params.id}`;
+
+  burger.update(burgerCondition, (result) => {
+    console.log(result);
+    if (result.changedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
 
 module.exports = router;
