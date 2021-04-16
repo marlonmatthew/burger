db42447805f7a73e1e2f531fa1bc9b5cd23043e8
Marlon Matthew <mbm.drums@gmail.com>
2021-04-16 12:46:07 -0700

    added jawsdb code in connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ const connection = mysql.createConnection({
   database: "burgers_db",
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db",
+  });
+}
+
 connection.connect((err) => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
